b0323e392a24650731bbf18d92305614f9bf777d
Harald Maida <harald.maida@innoave.com>
2018-12-04 22:19:50 +0100

    solve day 4 part 1

---
+++ src/day04/mod.rs
@@ -0,0 +1,221 @@
+//! # Day 4: Repose Record
+//!
+//! ## Part 1
+//!
+//! You've sneaked into another supply closet - this time, it's across from the
+//! prototype suit manufacturing lab. You need to sneak inside and fix the
+//! issues with the suit, but there's a guard stationed outside the lab, so this
+//! is as close as you can safely get.
+//!
+//! As you search the closet for anything that might help, you discover that
+//! you're not the first person to want to sneak in. Covering the walls, someone
+//! has spent an hour starting every midnight for the past few months secretly
+//! observing this guard post! They've been writing down the ID of the one guard
+//! on duty that night - the Elves seem to have decided that one guard was
+//! enough for the overnight shift - as well as when they fall asleep or wake up
+//! while at their post (your puzzle input).
+//!
+//! For example, consider the following records, which have already been
+//! organized into chronological order:
+//!
+//! ```text
+//! [1518-11-01 00:00] Guard #10 begins shift
+//! [1518-11-01 00:05] falls asleep
+//! [1518-11-01 00:25] wakes up
+//! [1518-11-01 00:30] falls asleep
+//! [1518-11-01 00:55] wakes up
+//! [1518-11-01 23:58] Guard #99 begins shift
+//! [1518-11-02 00:40] falls asleep
+//! [1518-11-02 00:50] wakes up
+//! [1518-11-03 00:05] Guard #10 begins shift
+//! [1518-11-03 00:24] falls asleep
+//! [1518-11-03 00:29] wakes up
+//! [1518-11-04 00:02] Guard #99 begins shift
+//! [1518-11-04 00:36] falls asleep
+//! [1518-11-04 00:46] wakes up
+//! [1518-11-05 00:03] Guard #99 begins shift
+//! [1518-11-05 00:45] falls asleep
+//! [1518-11-05 00:55] wakes up
+//! ```
+//!
+//! Timestamps are written using year-month-day hour:minute format. The guard
+//! falling asleep or waking up is always the one whose shift most recently
+//! started. Because all asleep/awake times are during the midnight hour
+//! (00:00 - 00:59), only the minute portion (00 - 59) is relevant for those
+//! events.
+//!
+//! Visually, these records show that the guards are asleep at these times:
+//!
+//! ```text
+//! Date   ID   Minute
+//!             000000000011111111112222222222333333333344444444445555555555
+//!             012345678901234567890123456789012345678901234567890123456789
+//! 11-01  #10  .....####################.....#########################.....
+//! 11-02  #99  ........................................##########..........
+//! 11-03  #10  ........................#####...............................
+//! 11-04  #99  ....................................##########..............
+//! 11-05  #99  .............................................##########.....
+//! ```
+//!
+//! The columns are Date, which shows the month-day portion of the relevant day;
+//! ID, which shows the guard on duty that day; and Minute, which shows the
+//! minutes during which the guard was asleep within the midnight hour. (The
+//! Minute column's header shows the minute's ten's digit in the first row and
+//! the one's digit in the second row.) Awake is shown as ., and asleep is shown
+//! as #.
+//!
+//! Note that guards count as asleep on the minute they fall asleep, and they
+//! count as awake on the minute they wake up. For example, because Guard #10
+//! wakes up at 00:25 on 1518-11-01, minute 25 is marked as awake.
+//!
+//! If you can figure out the guard most likely to be asleep at a specific time,
+//! you might be able to trick that guard into working tonight so you can have
+//! the best chance of sneaking in. You have two strategies for choosing the
+//! best guard/minute combination.
+//!
+//! **Strategy 1:** Find the guard that has the most minutes asleep. What minute
+//! does that guard spend asleep the most?
+//!
+//! In the example above, Guard #10 spent the most minutes asleep, a total of
+//! 50 minutes (20+25+5), while Guard #99 only slept for a total of 30 minutes
+//! (10+10+10). Guard #10 was asleep most during minute 24 (on two days, whereas
+//! any other minute the guard was asleep was only seen on one day).
+//!
+//! While this example listed the entries in chronological order, your entries
+//! are in the order you found them. You'll need to organize them before they
+//! can be analyzed.
+//!
+//! What is the ID of the guard you chose multiplied by the minute you chose?
+//! <br/>(In the above example, the answer would be 10 * 24 = 240.)
+//!
+//! [Advent of Code 2018 - Day 4](https://adventofcode.com/2018/day/4)
+
+use std::collections::HashMap;
+use std::u8;
+
+pub type GuardId = u16;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Event {
+    FallAsleep,
+    WakeUp,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Record {
+    pub guard_id: GuardId,
+    pub day: String,
+    pub hour: u8,
+    pub minute: u8,
+    pub event: Event,
+}
+
+#[aoc_generator(day4)]
+pub fn parse(input: &str) -> Vec<Record> {
+    let mut lines: Vec<_> = input.lines().collect();
+    lines.sort();
+    let mut records = Vec::with_capacity(16);
+    let mut guard_id = 0;
+    for line in lines {
+        match &line[19..24] {
+            "Guard" => {
+                guard_id = line[26..]
+                    .chars()
+                    .filter(|c| c.is_digit(10))
+                    .collect::<String>()
+                    .parse()
+                    .expect(&format!("not a guard id in line {}", line))
+            }
+            "falls" => {
+                let day = line[1..11].to_owned();
+                let hour = line[12..14]
+                    .parse()
+                    .expect(&format!("not an hour in line {}", line));
+                let minute = line[15..17]
+                    .parse()
+                    .expect(&format!("not an minute in line {}", line));
+                records.push(Record {
+                    guard_id,
+                    day,
+                    hour,
+                    minute,
+                    event: Event::FallAsleep,
+                })
+            }
+            "wakes" => {
+                let day = line[1..11].to_owned();
+                let hour = line[12..14]
+                    .parse()
+                    .expect(&format!("not an hour in line {}", line));
+                let minute = line[15..17]
+                    .parse()
+                    .expect(&format!("not an minute in line {}", line));
+                records.push(Record {
+                    guard_id,
+                    day,
+                    hour,
+                    minute,
+                    event: Event::WakeUp,
+                })
+            }
+            _ => unreachable!(&format!("invalid line: {}", line)),
+        }
+    }
+    records
+}
+
+#[aoc(day4, part1)]
+pub fn strategy1(input: &[Record]) -> u32 {
+    let (guard_id, minute) = most_asleep_minute(input);
+    u32::from(guard_id) * minute
+}
+
+pub fn most_asleep_minute(input: &[Record]) -> (GuardId, u32) {
+    let mut sleeping_periods: HashMap<GuardId, Vec<(u8, u8)>> = HashMap::with_capacity(16);
+    let mut time_sleeping: HashMap<GuardId, u32> = HashMap::with_capacity(16);
+    let mut fall_asleep_time = 0;
+    for record in input {
+        match record.event {
+            Event::FallAsleep => fall_asleep_time = record.minute,
+            Event::WakeUp => {
+                let sleep_period = (fall_asleep_time, record.minute);
+                sleeping_periods
+                    .entry(record.guard_id)
+                    .and_modify(|times| times.push(sleep_period))
+                    .or_insert_with(|| vec![sleep_period]);
+                let sleep_time = u32::from(record.minute - fall_asleep_time);
+                time_sleeping
+                    .entry(record.guard_id)
+                    .and_modify(|minutes| *minutes += sleep_time)
+                    .or_insert(sleep_time);
+            }
+        }
+    }
+
+    let most_sleepy_guard = time_sleeping
+        .into_iter()
+        .max_by_key(|(_, value)| *value)
+        .map(|(guard_id, _)| guard_id)
+        .expect(&format!("what? no guard sleeps actually?"));
+
+    let mut sleepy_minutes = HashMap::with_capacity(32);
+    for (sleeps_from, sleeps_until) in sleeping_periods[&most_sleepy_guard].iter() {
+        for minute in *sleeps_from..*sleeps_until {
+            sleepy_minutes
+                .entry(minute)
+                .and_modify(|times| *times += 1)
+                .or_insert(1);
+        }
+    }
+
+    let ovl_minute = sleepy_minutes
+        .into_iter()
+        .max_by_key(|(_, times)| *times)
+        .map(|(minute, _)| minute)
+        .expect("it was promised that the input reduces to exactly one minute");
+
+    (most_sleepy_guard, u32::from(ovl_minute))
+}
+
+#[cfg(test)]
+mod tests;
---
+++ src/day04/tests.rs
@@ -0,0 +1,68 @@
+use super::*;
+
+#[test]
+fn parse_falls_asleep() {
+    let input = "[1518-05-19 23:51] Guard #577 begins shift\n[1518-05-20 00:03] falls asleep";
+
+    let records = parse(input);
+
+    assert_eq!(records.len(), 1);
+    assert_eq!(
+        records[0],
+        Record {
+            guard_id: 577,
+            day: "1518-05-20".into(),
+            hour: 0,
+            minute: 3,
+            event: Event::FallAsleep
+        }
+    )
+}
+
+#[test]
+fn parse_wakes_up() {
+    let input = "[1518-07-18 00:00] Guard #1289 begins shift\n[1518-07-19 00:15] wakes up";
+
+    let records = parse(input);
+
+    assert_eq!(records.len(), 1);
+    assert_eq!(
+        records[0],
+        Record {
+            guard_id: 1289,
+            day: "1518-07-19".into(),
+            hour: 0,
+            minute: 15,
+            event: Event::WakeUp
+        }
+    )
+}
+
+mod part1 {
+    use super::*;
+
+    #[test]
+    fn example1() {
+        let input = "[1518-11-01 00:00] Guard #10 begins shift
+[1518-11-01 00:05] falls asleep
+[1518-11-01 00:25] wakes up
+[1518-11-01 00:30] falls asleep
+[1518-11-01 00:55] wakes up
+[1518-11-01 23:58] Guard #99 begins shift
+[1518-11-02 00:40] falls asleep
+[1518-11-02 00:50] wakes up
+[1518-11-03 00:05] Guard #10 begins shift
+[1518-11-03 00:24] falls asleep
+[1518-11-03 00:29] wakes up
+[1518-11-04 00:02] Guard #99 begins shift
+[1518-11-04 00:36] falls asleep
+[1518-11-04 00:46] wakes up
+[1518-11-05 00:03] Guard #99 begins shift
+[1518-11-05 00:45] falls asleep
+[1518-11-05 00:55] wakes up";
+
+        let answer = most_asleep_minute(&parse(input));
+
+        assert_eq!(answer, (10, 24));
+    }
+}
